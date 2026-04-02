44859d3f0c817dbb45921da403a9879da583374d
pavithran406 <pavithranmaha1@gmail.com>
2026-04-02 14:29:23 +0530

    home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import './App.css'
-import {BrowserRouter as Router , Route,Routes} from "react-router-dom"
-import Navbar from './component/Navbar'
-import Home from './component/Home'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
 function App() {
-
   return (
-  <>
-
-
-  <Router>
-    <Navbar/>
- <Routes>
-  <Route path='/' element={<Home/>}/>
- </Routes>
-  </Router>
-
-
-
-  </>
-  )
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
